b2bbcbd416570269678998570c9eac6276722de3
rustyrohbot <rustyrohbot@protonmail.com>
2025-01-09 19:09:56 -0600

    adding photos

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My Website';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Rohbot Dot Dev';
+export const SITE_DESCRIPTION = 'My corner of the internet.';
 export const ABOUT_PAGE_TITLE = 'About Me';
-export const ABOUT_PAGE_DESCRIPTION = 'Learn more about me and my work.';
+export const ABOUT_PAGE_DESCRIPTION = 'A brief description of me.';
